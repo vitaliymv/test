83b2ba6e5c763102ef1cfceececa433feaa5a411
Vitalii <movcan77@gmail.com>
2020-03-10 23:15:05 +0200

    First server on Spring

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/test")
+    String hello() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
